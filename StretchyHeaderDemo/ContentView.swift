15fda964837909410a84d4ce1fd733216cf69e40
Thomas Frank <t.ostlyng@outlook.com>
2025-09-02 18:55:02 +0200

    Initial Commit

---
+++ StretchyHeaderDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StretchyHeaderDemo
+//
+//  Created by Thomas Østlyng on 2/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StretchyHeaderDemo/StretchyHeaderDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StretchyHeaderDemoApp.swift
+//  StretchyHeaderDemo
+//
+//  Created by Thomas Østlyng on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct StretchyHeaderDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
